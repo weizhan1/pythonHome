a7b55da55e186fa5ec6f39a6bc4f3e8a327d8748
weizhan1 <weizhan@gmail.com>
2015-02-04 17:13:19 -0800

    Update Interview2.py

--- Interview2.py
+++ Interview2.py
@@ -10,3 +10,13 @@ s = "1a2b3c"
 num = re.findall('-\d+|\d+', s)
 strint = [int(x) for x in num]
 sum(strint)
+
+>>> first_chars = [word[0] for word in itertools.takewhile(lambda word: word != "and", text.split())]
+
+from itertools import groupby
+
+def get_category(line):
+    return line.split("/")[0]
+
+with open("/var/lib/portage/world") as worldfile:
+    packages = {category: list(packages) for category, packages in groupby(worldfile, get_category)}
