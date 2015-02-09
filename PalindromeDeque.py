a55e2ec63eed2ceb61628caeb9ee60d48521cbd9
weizhan1 <weizhan@gmail.com>
2015-02-08 21:34:13 -0800

    Update PalindromeDeque.py

--- PalindromeDeque.py
+++ PalindromeDeque.py
@@ -18,3 +18,23 @@ def palchecker(aString):
 
 print(palchecker("lsdkjfskf"))
 print(palchecker("radar"))
+
+from collections import deque
+def palchecker(aString):
+    chardeque = deque()
+
+    for ch in aString:
+        chardeque.append(ch)
+
+    stillEqual = True
+
+    while len(chardeque) > 1 and stillEqual:
+        first = chardeque.popleft()
+        last = chardeque.pop()
+        if first != last:
+            stillEqual = False
+
+    return stillEqual
+
+print(palchecker("lsdkjfskf"))
+print(palchecker("radar"))
